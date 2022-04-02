e44c921362fecd88cc9587a6be61c1caef845c17
huycq1712 <huysk82000@gmail.com>
2022-04-02 21:26:13 +0700

    update data

--- data/build.py
+++ data/build.py
@@ -4,14 +4,19 @@
 @contact: sherlockliao01@gmail.com
 """
 
+from data.collate_batch import collate_fn
 from torch.utils import data
 
-from .datasets.mnist import MNIST
+from .datasets.MotionDataset import MotionDataset
 from .transforms import build_transforms
 
 
-def build_dataset(transforms, is_train=True):
-    datasets = MNIST(root='./', train=is_train, transform=transforms, download=True)
+def build_dataset(cfg, transforms, is_train=True):
+    datasets = MotionDataset(root=cfg.ROOT, 
+                             ann_file=cfg.ANNO_FILE, 
+                             transforms=transforms)
+    if not is_train:
+        return datasets
     return datasets
 
 
@@ -24,11 +29,11 @@ def make_data_loader(cfg, is_train=True):
         shuffle = False
 
     transforms = build_transforms(cfg, is_train)
-    datasets = build_dataset(transforms, is_train)
+    datasets = build_dataset(cfg, transforms, is_train)
 
     num_workers = cfg.DATALOADER.NUM_WORKERS
     data_loader = data.DataLoader(
-        datasets, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers
+        datasets, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, collate_fn = collate_fn
     )
 
     return data_loader
--- data/collate_batch.py
+++ data/collate_batch.py
@@ -3,3 +3,10 @@
 @author:  sherlock
 @contact: sherlockliao01@gmail.com
 """
+import torch
+
+def collate_fn(batch):
+    transposed_batch = list(zip(*batch))
+    images = torch.stack(transposed_batch[0])
+    
+    return images
